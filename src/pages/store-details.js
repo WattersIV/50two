4764947df01346ef59419d586dd3698814571734
Bill Watters <billywatters13@gmail.com>
2021-05-04 19:39:32 -0400

    Add google map to information page

--- src/pages/store-details.js
+++ src/pages/store-details.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import NavBar from '../components/NavBar'
-
+import { GoogleMap } from '../components/GoogleMap'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -11,6 +11,7 @@ const StoreDetails = () => (
     <h1>Store Details</h1>
     <body>
       <NavBar />
+      <GoogleMap />
     </body>
   </Layout>
 )
