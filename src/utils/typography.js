8b82db59178c6fe5b25a4d0db1afb88cbd91a868
Bill Watters <billywatters13@gmail.com>
2021-05-20 19:48:29 -0400

    Add new fonts

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+import stAnnesTheme from 'typography-theme-st-annes'
+const typography = new Typography(stAnnesTheme);
+
+typography.injectStyles();
+
+export default typography;
