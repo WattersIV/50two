b38eccd24fa42862960b2832cba6dc7827aff6fe
Bill Watters <billywatters13@gmail.com>
2021-05-23 22:38:23 -0400

    Hide hamburger when container out of view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,49 +1,53 @@
-import React, { useState } from 'react'
-import './navBar.css'
-import { navItems } from '../menu'
-import { Link } from 'gatsby'
+import React, { useState } from "react"
+import "./navBar.css"
+import { menu, navItems } from "../menu"
+import { Link } from "gatsby"
+import useIsInViewport from "use-is-in-viewport"
 
 export default function NavBar(props) {
   const { navOpen, setNavOpen } = props
+  const [isInViewport, navRef] = useIsInViewport() // !isInViewport means it should be shown
   const changeBurgerState = () => {
     setNavOpen(!navOpen)
-    
   }
+  console.log(isInViewport)
   return (
-    <>
-    {/* Check size of viewport to make Hamburger conditional conditional */}
-      <div
-      id="nav-icon2"
-      onClick={() => changeBurgerState()} 
-      className={navOpen === true ? 'open' : ''}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      {navOpen && (
-        <div className='nav-dropdown'>
-          <ul style={{ marginLeft: 0 }}>
-            {navItems.map((item) => {
-              const location = 
-              item.title === 'Store Details' ? 
-              '/store-details' 
-              : `/#${item.title}`
-              return (
-                <Link to={location} className='nav-item'>
-                  <li key={`item-${item.title}`} className='nav-item'>
-                    {item.title}
-                  </li>
-
-                </Link>
-              )
-            })}
-          </ul>
-        </div>
+    <div ref={navRef}>
+      {!isInViewport && (
+        <>
+          <div
+            id="nav-icon2"
+            onClick={() => changeBurgerState()}
+            className={navOpen === true ? "open" : ""}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          {navOpen && (
+            <div className="nav-dropdown">
+              <ul style={{ marginLeft: 0 }}>
+                {navItems.map(item => {
+                  const location =
+                    item.title === "Store Details"
+                      ? "/store-details"
+                      : `/#${item.title}`
+                  return (
+                    <Link to={location} className="nav-item">
+                      <li key={`item-${item.title}`} className="nav-item">
+                        {item.title}
+                      </li>
+                    </Link>
+                  )
+                })}
+              </ul>
+            </div>
+          )}
+        </>
       )}
-    </>
+    </div>
   )
 }
